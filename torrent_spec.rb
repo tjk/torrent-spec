6d143367ea7c58849b4332e000654aa394a12cfb
TJ Koblentz <tj.koblentz@gmail.com>
2016-01-19 23:05:42 -0800

    get a little commit in

--- torrent_spec.rb
+++ torrent_spec.rb
@@ -1,9 +1,19 @@
 require_relative './spec_helper.rb'
 
+require 'json'
+
 RSpec.describe 'Torrent Client' do
   before(:all) do
     ClientRunner.start
   end
 
-  it 'should work'
+  let(:http) { Net::HTTP.new('localhost', 8000) }
+
+  it 'responds to ping request' do
+    res = http.get('/api/ping')
+    expect(res.code.to_i).to eq(200)
+    expect(JSON.parse(res.body)).to eq({
+      'ok' => true,
+    })
+  end
 end
